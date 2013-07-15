344ba3a66d67aebe8a862f56440e6e5d53201a57
vlad.alexeev@gmail.com <vlad.alexeev@gmail.com@83b7ce2f-46e5-b878-b427-490d49653a27>
2013-07-15 20:33:57 +0000

    Triangle grid - rows aligned.

--- trunk/grid-paint/static/js/grids.js
+++ trunk/grid-paint/static/js/grids.js
@@ -1,13 +1,15 @@
+var gridAttrs={
+	"fill":"#f0f0f0",
+	"stroke": "#808080"
+}
+
 function GridSquare() {
 	this.cellSize=24;
 	this.paintGrid=function (paper) {
 		for (var y=0; y<(paper.height-this.cellSize); y+=this.cellSize) {
 			for (var x=0; x<(paper.width-this.cellSize); x+=this.cellSize) {
 				var r=paper.rect(x, y, this.cellSize, this.cellSize);
-				r.attr({
-					"fill":"#ffffff",
-					"stroke": "#808080"
-					});
+				r.attr(gridAttrs);
 			}			
 		}
 	}
@@ -17,24 +19,30 @@ function GridTriangle () {
 	this.cellSize=24;
 	this.paintGrid=function (paper) {
 		var rowHeight=this.cellSize*Math.sin(60/180*Math.PI);
-		for (var y=0; y<(paper.height-rowHeight); y+=rowHeight) {
+		var rowIndex=0;
+		for (var y=0; y<(paper.height-rowHeight); y+=rowHeight, rowIndex++) {
 			for (var x=0; x<(paper.width-this.cellSize*1.5); x+=this.cellSize) {
-				var path1=paper.path(
-					"M "+x+" "+(y+rowHeight)+" L "+(x+this.cellSize/2)+" "+y+
-					" L "+(x+this.cellSize)+" "+(y+rowHeight)+" Z");
-				path1.attr({
-					"fill":"#ffffff",
-					"stroke": "#808080"
-					});
+				if (rowIndex % 2 == 0) {
+					var path1=paper.path(
+						"M "+x+" "+(y+rowHeight)+" L "+(x+this.cellSize/2)+" "+y+
+						" L "+(x+this.cellSize)+" "+(y+rowHeight)+" Z");
+					path1.attr(gridAttrs);
 					
-				var path2=paper.path(
-					"M "+(x+this.cellSize/2)+" "+y+" L "+(x+this.cellSize)+" "+(y+rowHeight)+
-					" L "+(x+this.cellSize*1.5)+" "+y+" Z"
-				);
-				path2.attr({
-					"fill":"#ffffff",
-					"stroke": "#808080"
-					});
+					var path2=paper.path(
+						"M "+(x+this.cellSize/2)+" "+y+" L "+(x+this.cellSize)+" "+(y+rowHeight)+
+						" L "+(x+this.cellSize*1.5)+" "+y+" Z");
+					path2.attr(gridAttrs);
+				} else {
+					var path1=paper.path(
+						"M "+x+" "+y+" L "+(x+this.cellSize/2)+" "+(y+rowHeight)+
+						" L "+(x+this.cellSize)+" "+y+" Z");
+					path1.attr(gridAttrs);
+					
+					var path2=paper.path(
+						"M "+(x+this.cellSize/2)+" "+(y+rowHeight)+" L "+(x+this.cellSize)+" "+y+
+						" L "+(x+this.cellSize*1.5)+" "+(y+rowHeight)+" Z");
+					path2.attr(gridAttrs);
+				}
 			}
 		}
 	}
