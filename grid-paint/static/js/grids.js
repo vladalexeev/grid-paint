feb499d48302df401da365ef4327e3a2e932c386
vlad.alexeev@gmail.com <vlad.alexeev@gmail.com@83b7ce2f-46e5-b878-b427-490d49653a27>
2013-07-18 10:33:07 +0000

    Drwing hex grid by dashed lines

--- grid-paint/static/js/grids.js
+++ grid-paint/static/js/grids.js
@@ -3,6 +3,14 @@ var gridAttrs={
 	"stroke": "#808080"
 }
 
+var sin30=Math.sin(Math.PI/6);
+var sin60=Math.sin(60/180*Math.PI);
+var cos30=Math.cos(30/180*Math.PI);
+var cos60=Math.cos(60/180*Math.PI);
+var tan30=Math.tan(30/180*Math.PI);
+var tan60=Math.tan(60/180*Math.PI);
+
+
 function GridSquare() {
 	this.cellSize=24;
 	this.paintGrid=function (paper) {
@@ -51,9 +59,12 @@ function GridTriangle () {
 function GridHex() {
 	this.cellSize=24;
 	this.paintGrid=function(paper) {
+	    var paperHeight=paper.height;
+	    var paperWidth=paper.width;
 		var sideLength=this.cellSize/2;
-		var halfHeight=sideLength*Math.sin(60/180*Math.PI);
-		var dx=sideLength*Math.cos(60/180*Math.PI);
+		var halfHeight=sideLength*sin60;
+		var dx=sideLength*cos60;
+				/*
 		var rowIndex=0;
 		for (y=0; y<=(paper.height-2*halfHeight); y+=halfHeight) {
 			var x=0;
@@ -74,6 +85,74 @@ function GridHex() {
 			}
 			
 			rowIndex++
+		}*/
+
+		for (y=0; y<paper.height; y+=halfHeight*2) {
+		    var path=paper.path(["M ",dx,y,"H",paper.width]);
+		    path.attr({
+		        "stroke":"#ff0000",
+		    })
+		    path.node.setAttribute("stroke-dasharray","12,24");
 		}
+		
+		for (y=halfHeight; y<paper.height; y+=halfHeight*2) {
+		    var path=paper.path(["M",2*dx+sideLength,y,"H",paper.width]);
+		    path.attr({
+                "stroke":"#ff0000",
+            })
+            path.node.setAttribute("stroke-dasharray","12,24");
+		}
+		
+		var slantedLinesStep=2*dx+2*sideLength;
+		var hexBehind=Math.floor(paperHeight/slantedLinesStep);
+		
+		for (x=dx+sideLength-hexBehind*slantedLinesStep; x<paper.width; x+=slantedLinesStep) {
+		    var path=paper.path(["M",x,0,"L",x+paperHeight*tan30,paperHeight]);
+            path.attr({
+                "stroke":"#ff0000",
+            })
+            path.node.setAttribute("stroke-dasharray","12,24");
+		}
+		
+		for (x=0-hexBehind*slantedLinesStep; x<paper.width; x+=slantedLinesStep) {
+            var path=paper.path(["M",x,halfHeight,"L",x+paperHeight*tan30,halfHeight+paperHeight]);
+            path.attr({
+                "stroke":"#ff0000",
+            })
+            path.node.setAttribute("stroke-dasharray","12,24");
+        }
+        
+        for (x=dx+sideLength-hexBehind*slantedLinesStep; x<paper.width; x+=slantedLinesStep) {
+            var path=paper.path(["M",x,halfHeight*2,"L",x+paperHeight*tan30,halfHeight*2+paperHeight]);
+            path.attr({
+                "stroke":"#ff0000",
+            })
+            path.node.setAttribute("stroke-dasharray","12,24");
+        }
+        
+		
+		for (x=dx; x<paper.width+hexBehind*slantedLinesStep; x+=slantedLinesStep) {
+            var path=paper.path(["M",x,0,"L",x-paperHeight*tan30,paperHeight]);
+            path.attr({
+                "stroke":"#ff0000",
+            })
+            path.node.setAttribute("stroke-dasharray","12,24");
+        }
+        
+        for (x=2*dx+sideLength; x<paper.width+hexBehind*slantedLinesStep; x+=slantedLinesStep) {
+            var path=paper.path(["M",x,halfHeight,"L",x-paperHeight*tan30,halfHeight+paperHeight]);
+            path.attr({
+                "stroke":"#ff0000",
+            })
+            path.node.setAttribute("stroke-dasharray","12,24");
+        }
+        
+        for (x=dx; x<paper.width+hexBehind*slantedLinesStep; x+=slantedLinesStep) {
+            var path=paper.path(["M",x,2*halfHeight,"L",x-paperHeight*tan30,2*halfHeight+paperHeight]);
+            path.attr({
+                "stroke":"#ff0000",
+            })
+            path.node.setAttribute("stroke-dasharray","12,24");
+        }
 	}
 }
