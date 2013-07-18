267610a06b50690c5b37a326f43272e8f0e2a055
vlad.alexeev@gmail.com <vlad.alexeev@gmail.com@83b7ce2f-46e5-b878-b427-490d49653a27>
2013-07-18 17:43:47 +0000

    All grids are pained by lines.

--- grid-paint/static/js/grids.js
+++ grid-paint/static/js/grids.js
@@ -1,8 +1,3 @@
-var gridAttrs={
-	"fill":"#f0f0f0",
-	"stroke": "#808080"
-}
-
 var sin30=Math.sin(Math.PI/6);
 var sin60=Math.sin(60/180*Math.PI);
 var cos30=Math.cos(30/180*Math.PI);
@@ -10,54 +5,78 @@ var cos60=Math.cos(60/180*Math.PI);
 var tan30=Math.tan(30/180*Math.PI);
 var tan60=Math.tan(60/180*Math.PI);
 
+/*
+ * Interface of Grid
+ *   function paintGrid(paper) - paints grid on a paper 
+ * 
+ * 
+ */
+
 
 function GridSquare() {
 	this.cellSize=24;
+	
+	this._createGridLine=function(paper, pathArray) {
+		var path=paper.path(pathArray);
+        path.attr({
+            "stroke":"#d0d0d0",
+        })
+	}
+	
 	this.paintGrid=function (paper) {
-		for (var y=0; y<=(paper.height-this.cellSize); y+=this.cellSize) {
-			for (var x=0; x<=(paper.width-this.cellSize); x+=this.cellSize) {
-				var r=paper.rect(x, y, this.cellSize, this.cellSize);
-				r.attr(gridAttrs);
-			}			
+		for (var y=0; y<paper.height; y+=this.cellSize) {
+			this._createGridLine(paper,["M",0,y,"L",paper.width,y]);
 		}
+		
+		for (var x=0; x<paper.width; x+=this.cellSize) {
+			this._createGridLine(paper,["M",x,0,"L",x,paper.height]);
+		}			
+		
 	}
 }
 
 function GridTriangle () {
 	this.cellSize=24;
+	
+	this._createGridLine=function(paper, pathArray) {
+		var path=paper.path(pathArray);
+        path.attr({
+            "stroke":"#d0d0d0",
+        })
+	}
+		
 	this.paintGrid=function (paper) {
 		var rowHeight=this.cellSize*Math.sin(60/180*Math.PI);
-		var rowIndex=0;
-		for (var y=0; y<(paper.height-rowHeight); y+=rowHeight, rowIndex++) {
-			for (var x=0; x<(paper.width-this.cellSize*1.5); x+=this.cellSize) {
-				if (rowIndex % 2 == 0) {
-					var path1=paper.path(
-						"M "+x+" "+(y+rowHeight)+" L "+(x+this.cellSize/2)+" "+y+
-						" L "+(x+this.cellSize)+" "+(y+rowHeight)+" Z");
-					path1.attr(gridAttrs);
-					
-					var path2=paper.path(
-						"M "+(x+this.cellSize/2)+" "+y+" L "+(x+this.cellSize)+" "+(y+rowHeight)+
-						" L "+(x+this.cellSize*1.5)+" "+y+" Z");
-					path2.attr(gridAttrs);
-				} else {
-					var path1=paper.path(
-						"M "+x+" "+y+" L "+(x+this.cellSize/2)+" "+(y+rowHeight)+
-						" L "+(x+this.cellSize)+" "+y+" Z");
-					path1.attr(gridAttrs);
-					
-					var path2=paper.path(
-						"M "+(x+this.cellSize/2)+" "+(y+rowHeight)+" L "+(x+this.cellSize)+" "+y+
-						" L "+(x+this.cellSize*1.5)+" "+(y+rowHeight)+" Z");
-					path2.attr(gridAttrs);
-				}
-			}
+		var trianglesBehind=Math.floor(paper.height/this.cellSize)
+				
+		for (var y=0; y<paper.height; y+=rowHeight) {
+			this._createGridLine(paper,["M",0,y,"H",paper.width]);
 		}
+		
+		for (var x=this.cellSize/2-trianglesBehind*this.cellSize; x<paper.width; x+=this.cellSize) {
+			this._createGridLine(paper,["M",x,0,"L",x+paper.height*tan30,paper.height]);
+		}
+		
+		for (var x=this.cellSize/2; x<paper.width+trianglesBehind*this.cellSize; x+=this.cellSize) {
+			this._createGridLine(paper,["M",x,0,"L",x-paper.height*tan30,paper.height]);
+		}
+
 	}
 }
 
 function GridHex() {
 	this.cellSize=24;
+	this._sideLength=this.cellSize/2;
+	this._stroke_dash_array=this._sideLength+","+this.cellSize;
+	
+	this._createGridLine=function(paper, pathArray) {
+		var path=paper.path(pathArray);
+        path.attr({
+            "stroke":"#d0d0d0",
+        })
+        path.node.setAttribute("stroke-dasharray",this._stroke_dash_array);
+	}
+	
 	this.paintGrid=function(paper) {
 	    var paperHeight=paper.height;
 	    var paperWidth=paper.width;
@@ -88,71 +107,39 @@ function GridHex() {
 		}*/
 
 		for (y=0; y<paper.height; y+=halfHeight*2) {
-		    var path=paper.path(["M ",dx,y,"H",paper.width]);
-		    path.attr({
-		        "stroke":"#ff0000",
-		    })
-		    path.node.setAttribute("stroke-dasharray","12,24");
+		    this._createGridLine(paper,["M ",dx,y,"H",paper.width]);
 		}
 		
 		for (y=halfHeight; y<paper.height; y+=halfHeight*2) {
-		    var path=paper.path(["M",2*dx+sideLength,y,"H",paper.width]);
-		    path.attr({
-                "stroke":"#ff0000",
-            })
-            path.node.setAttribute("stroke-dasharray","12,24");
+		    this._createGridLine(paper,["M",2*dx+sideLength,y,"H",paper.width]);
 		}
 		
 		var slantedLinesStep=2*dx+2*sideLength;
 		var hexBehind=Math.floor(paperHeight/slantedLinesStep);
 		
 		for (x=dx+sideLength-hexBehind*slantedLinesStep; x<paper.width; x+=slantedLinesStep) {
-		    var path=paper.path(["M",x,0,"L",x+paperHeight*tan30,paperHeight]);
-            path.attr({
-                "stroke":"#ff0000",
-            })
-            path.node.setAttribute("stroke-dasharray","12,24");
+		    this._createGridLine(paper,["M",x,0,"L",x+paperHeight*tan30,paperHeight]);
 		}
 		
 		for (x=0-hexBehind*slantedLinesStep; x<paper.width; x+=slantedLinesStep) {
-            var path=paper.path(["M",x,halfHeight,"L",x+paperHeight*tan30,halfHeight+paperHeight]);
-            path.attr({
-                "stroke":"#ff0000",
-            })
-            path.node.setAttribute("stroke-dasharray","12,24");
+            this._createGridLine(paper,["M",x,halfHeight,"L",x+paperHeight*tan30,halfHeight+paperHeight]);
         }
         
         for (x=dx+sideLength-hexBehind*slantedLinesStep; x<paper.width; x+=slantedLinesStep) {
-            var path=paper.path(["M",x,halfHeight*2,"L",x+paperHeight*tan30,halfHeight*2+paperHeight]);
-            path.attr({
-                "stroke":"#ff0000",
-            })
-            path.node.setAttribute("stroke-dasharray","12,24");
+            this._createGridLine(paper,["M",x,halfHeight*2,"L",x+paperHeight*tan30,halfHeight*2+paperHeight]);
         }
         
 		
 		for (x=dx; x<paper.width+hexBehind*slantedLinesStep; x+=slantedLinesStep) {
-            var path=paper.path(["M",x,0,"L",x-paperHeight*tan30,paperHeight]);
-            path.attr({
-                "stroke":"#ff0000",
-            })
-            path.node.setAttribute("stroke-dasharray","12,24");
+            this._createGridLine(paper,["M",x,0,"L",x-paperHeight*tan30,paperHeight]);
         }
         
         for (x=2*dx+sideLength; x<paper.width+hexBehind*slantedLinesStep; x+=slantedLinesStep) {
-            var path=paper.path(["M",x,halfHeight,"L",x-paperHeight*tan30,halfHeight+paperHeight]);
-            path.attr({
-                "stroke":"#ff0000",
-            })
-            path.node.setAttribute("stroke-dasharray","12,24");
+            this._createGridLine(paper,["M",x,halfHeight,"L",x-paperHeight*tan30,halfHeight+paperHeight]);
         }
         
         for (x=dx; x<paper.width+hexBehind*slantedLinesStep; x+=slantedLinesStep) {
-            var path=paper.path(["M",x,2*halfHeight,"L",x-paperHeight*tan30,2*halfHeight+paperHeight]);
-            path.attr({
-                "stroke":"#ff0000",
-            })
-            path.node.setAttribute("stroke-dasharray","12,24");
+            this._createGridLine(paper,["M",x,2*halfHeight,"L",x-paperHeight*tan30,2*halfHeight+paperHeight]);
         }
 	}
 }
