1848b618a16463bec39e064af391765e13b5fa25
vlad.alexeev@gmail.com <vlad.alexeev@gmail.com@83b7ce2f-46e5-b878-b427-490d49653a27>
2013-07-25 17:23:03 +0000

    Drawing jewel shape on server

--- trunk/grid-paint/grid/square.py
+++ trunk/grid-paint/grid/square.py
@@ -44,13 +44,54 @@ class ShapeDiamond:
                        (x+grid.cell_size/2, y+grid.cell_size/2),
                        (x+grid.cell_size, y)],
                       fill=c4)
+        
+class ShapeJewel:
+    def paint(self, image, grid, x, y, color):
+        rgb=clr.hex_to_rgb(color)
+        hls=colorsys.rgb_to_hls(*rgb)
+        facet=grid.cell_size/6
+        
+        c1=clr.rgb256(*colorsys.hls_to_rgb(*clr.lighten_hls(hls, 0.2)))
+        image.polygon([(x, y),
+                       (x+facet, y+facet),
+                       (x+grid.cell_size-facet, y+facet),
+                       (x+grid.cell_size, y)],
+                      fill=c1)
+        
+        c2=clr.rgb256(*colorsys.hls_to_rgb(*clr.lighten_hls(hls, 0.1)))
+        image.polygon([(x, y),
+                       (x+facet, y+facet),
+                       (x+facet, y+grid.cell_size-facet),
+                       (x, y+grid.cell_size)],
+                      fill=c2)
+        
+        c3=clr.rgb256(*colorsys.hls_to_rgb(*clr.darken_hls(hls, 0.1)))
+        image.polygon([(x, y+grid.cell_size),
+                       (x+facet, y+grid.cell_size-facet),
+                       (x+grid.cell_size-facet, y+grid.cell_size-facet),
+                       (x+grid.cell_size, y+grid.cell_size)],
+                      fill=c3)
+        
+        c4=clr.rgb256(*colorsys.hls_to_rgb(*clr.darken_hls(hls, 0.2)))
+        image.polygon([(x+grid.cell_size, y),
+                       (x+grid.cell_size-facet, y+facet),
+                       (x+grid.cell_size-facet, y+grid.cell_size-facet),
+                       (x+grid.cell_size, y+grid.cell_size)],
+                      fill=c4)
+                
+        image.rectangle([x+facet,
+                         y+facet,
+                         x+grid.cell_size-facet-1,
+                         y+grid.cell_size-facet-1],
+                        fill=color)
        
 
 class GridSquare:
     cell_size=24;
     shapes={
             "flat": ShapeFlat(),
-            "diamond": ShapeDiamond()
+            "diamond": ShapeDiamond(),
+            "jewel": ShapeJewel()
             }
     def paintShape(self, image, jsonCell):
         x=jsonCell['col']*self.cell_size
