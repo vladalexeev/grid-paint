af434edf7ccc876bdec45a883c9823457bb3bbe4
vlad.alexeev@gmail.com <vlad.alexeev@gmail.com@83b7ce2f-46e5-b878-b427-490d49653a27>
2013-07-28 18:53:32 +0000

    drawing diamond triangle shape on server

--- grid-paint/grid/triangle.py
+++ grid-paint/grid/triangle.py
@@ -5,6 +5,11 @@ Created on 28.07.2013
 '''
 
 import math
+import colorsys
+
+import grid.color as clr
+
+
 
 sin60=math.sin(math.pi*60/180)
 
@@ -55,14 +60,43 @@ class ShapeFlat:
                        (pp[1]['x']+dx, pp[1]['y']+dy),
                        (pp[2]['x']+dx, pp[2]['y']+dy)],
                       fill=color)
+        
+class ShapeDiamond:
+    def __init__(self, grid):
+        self.grid=grid
+        
+    def paint(self, image, col, row, color, dx, dy):
+        rgb=clr.hex_to_rgb(color)
+        hls=colorsys.rgb_to_hls(*rgb)
+        pp=triangle_points(col, row, self.grid.cell_size);
 
+        
+        c1=clr.rgb256(*colorsys.hls_to_rgb(*clr.lighten_hls(hls, 0.2)))
+        image.polygon([(pp[0]['x']+dx, pp[0]['y']+dy),
+                       (pp[1]['x']+dx, pp[1]['y']+dy),
+                       (pp[3]['x']+dx, pp[3]['y']+dy)],
+                      fill=c1)
+        
+        c2=clr.rgb256(*colorsys.hls_to_rgb(*clr.lighten_hls(hls, 0.1)))
+        image.polygon([(pp[0]['x']+dx, pp[0]['y']+dy),
+                       (pp[2]['x']+dx, pp[2]['y']+dy),
+                       (pp[3]['x']+dx, pp[3]['y']+dy)],
+                      fill=c2)
+        
+        c3=clr.rgb256(*colorsys.hls_to_rgb(*clr.darken_hls(hls, 0.1)))
+        image.polygon([(pp[1]['x']+dx, pp[1]['y']+dy),
+                       (pp[2]['x']+dx, pp[2]['y']+dy),
+                       (pp[3]['x']+dx, pp[3]['y']+dy)],
+                      fill=c3)
+        
 
 class GridTriangle:
     cell_size=24
     
     def __init__(self):
         self.shapes={
-                     'flat': ShapeFlat(self)
+                     'flat': ShapeFlat(self),
+                     'diamond': ShapeDiamond(self)
                     }
     
     def paintShape(self, image, jsonCell, dx, dy):
