19210e8587d3cd01dc547e49f056394af8c2d068
vlad.alexeev@gmail.com <vlad.alexeev@gmail.com@83b7ce2f-46e5-b878-b427-490d49653a27>
2013-07-28 19:04:29 +0000

    Drawing Jewel triangle shape on server.

--- grid-paint/grid/triangle.py
+++ grid-paint/grid/triangle.py
@@ -89,6 +89,41 @@ class ShapeDiamond:
                        (pp[3]['x']+dx, pp[3]['y']+dy)],
                       fill=c3)
         
+class ShapeJewel:
+    def __init__(self, grid):
+        self.grid=grid
+        
+    def paint(self, image, col, row, color, dx, dy):
+        rgb=clr.hex_to_rgb(color)
+        hls=colorsys.rgb_to_hls(*rgb)
+        pp=triangle_points(col, row, self.grid.cell_size);
+        
+        c1=clr.rgb256(*colorsys.hls_to_rgb(*clr.lighten_hls(hls, 0.2)))
+        image.polygon([(pp[0]['x']+dx, pp[0]['y']+dy),
+                       (pp[1]['x']+dx, pp[1]['y']+dy),
+                       (pp[1]['x']+(pp[3]['x']-pp[1]['x'])/2+dx, pp[1]['y']+(pp[3]['y']-pp[1]['y'])/2+dy),
+                       (pp[0]['x']+(pp[3]['x']-pp[0]['x'])/2+dx, pp[0]['y']+(pp[3]['y']-pp[0]['y'])/2+dy)],
+                      fill=c1)
+
+        c2=clr.rgb256(*colorsys.hls_to_rgb(*clr.lighten_hls(hls, 0.1)))
+        image.polygon([(pp[0]['x']+dx, pp[0]['y']+dy),
+                       (pp[2]['x']+dx, pp[2]['y']+dy),
+                       (pp[2]['x']+(pp[3]['x']-pp[2]['x'])/2+dx, pp[2]['y']+(pp[3]['y']-pp[2]['y'])/2+dy),
+                       (pp[0]['x']+(pp[3]['x']-pp[0]['x'])/2+dx, pp[0]['y']+(pp[3]['y']-pp[0]['y'])/2+dy)],
+                      fill=c2)
+        
+        c3=clr.rgb256(*colorsys.hls_to_rgb(*clr.darken_hls(hls, 0.1)))
+        image.polygon([(pp[1]['x']+dx, pp[1]['y']+dy),
+                       (pp[2]['x']+dx, pp[2]['y']+dy),
+                       (pp[2]['x']+(pp[3]['x']-pp[2]['x'])/2+dx, pp[2]['y']+(pp[3]['y']-pp[2]['y'])/2+dy),
+                       (pp[1]['x']+(pp[3]['x']-pp[1]['x'])/2+dx, pp[1]['y']+(pp[3]['y']-pp[1]['y'])/2+dy)],
+                      fill=c3)
+
+        image.polygon([(pp[0]['x']+(pp[3]['x']-pp[0]['x'])/2+dx, pp[0]['y']+(pp[3]['y']-pp[0]['y'])/2+dy),
+                       (pp[1]['x']+(pp[3]['x']-pp[1]['x'])/2+dx, pp[1]['y']+(pp[3]['y']-pp[1]['y'])/2+dy),
+                       (pp[2]['x']+(pp[3]['x']-pp[2]['x'])/2+dx, pp[2]['y']+(pp[3]['y']-pp[2]['y'])/2+dy)],
+                      fill=color)
+        
 
 class GridTriangle:
     cell_size=24
@@ -96,7 +131,8 @@ class GridTriangle:
     def __init__(self):
         self.shapes={
                      'flat': ShapeFlat(self),
-                     'diamond': ShapeDiamond(self)
+                     'diamond': ShapeDiamond(self),
+                     'jewel': ShapeJewel(self)
                     }
     
     def paintShape(self, image, jsonCell, dx, dy):
