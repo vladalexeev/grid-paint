b32729f77f2f7728ed36b0f14117637fe62fe9e0
vlad.alexeev@gmail.com <vlad.alexeev@gmail.com@83b7ce2f-46e5-b878-b427-490d49653a27>
2013-08-15 19:24:28 +0000

    Color palette refactoring. Additional methods for color palette.

--- trunk/grid-paint/static/syronex-colorpicker/syronex-colorpicker.js
+++ trunk/grid-paint/static/syronex-colorpicker/syronex-colorpicker.js
@@ -32,66 +32,86 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 (function($) {
-  $.fn.colorPicker = function($$options) {
-    // Defaults
-    var $defaults = {
-      color:new Array(
-	"#FFFFFF", "#EEEEEE", "#FFFF88", "#FF7400", "#CDEB8B", "#6BBA70",
-	"#006E2E", "#C3D9FF", "#4096EE", "#356AA0", "#FF0096", "#B02B2C", 
-	"#000000"
-	),
-      defaultColor: 0,
-      columns: 0,
-      click: function($color){}
-    };
+  var methods = {
+    init : function( $$options ) { 
+      // Defaults
+      var $defaults = {
+        color:new Array(
+	       "#FFFFFF", "#EEEEEE", "#FFFF88", "#FF7400", "#CDEB8B", "#6BBA70",
+	       "#006E2E", "#C3D9FF", "#4096EE", "#356AA0", "#FF0096", "#B02B2C", 
+	       "#000000"),
+        defaultColor: 0,
+        columns: 0,
+        click: function($color){}
+      };
 		
-    var $settings = $.extend({}, $defaults, $$options);
+      var $settings = $.extend({}, $defaults, $$options);
 		
-    // Iterate and reformat each matched element
-    return this.each(function() {
+      // Iterate and reformat each matched element
+      return this.each(function() {
+        var $this = $(this);
+        // build element specific options
+        var o = $.meta ? $.extend({}, $settings, $this.data()) : $settings;
+        var $$oldIndex = typeof(o.defaultColor)=='number' ? o.defaultColor : -1;
+      
+        var _html = "";
+        for(i=0;i<o.color.length;i++){
+	      _html += '<div style="background-color:'+o.color[i]+';"></div>';
+	      if($$oldIndex==-1 && o.defaultColor==o.color[i]) $$oldIndex = i;
+        }
+      
+        $this.html('<div class="jColorSelect">'+_html+'</div>');
+        var $color = $this.children('.jColorSelect').children('div');
+        // Set container width
+        var w = ($color.width()+2+2) * (o.columns>0 ? o.columns : o.color.length );
+        //$this.children('.jColorSelect').css('width',w);
+      
+        // Subscribe to click event of each color box
+        $color.each(function(i){
+	      $(this).click(function(){
+	        if( $$oldIndex == i ) return;	  
+	        if( $$oldIndex > -1 ){
+	          cell = $color.eq($$oldIndex);
+	          if(cell.hasClass('check')) cell.removeClass(
+	             'check').removeClass('checkwht').removeClass('checkblk');
+	        }
+	        // Keep index
+	        $$oldIndex = i;
+	        $(this).addClass('check').addClass(isdark(o.color[i]) ? 'checkwht' : 'checkblk');
+	        // Trigger user event
+	        o.click(o.color[i]);
+	      });
+        });
+      
+        // Simulate click for defaultColor
+        _tmp = $$oldIndex;
+        $$oldIndex = -1;
+        $color.eq(_tmp).trigger('click');
+      });    
+      return this;
+    },
+    
+    colors: function() {
       var $this = $(this);
-      // build element specific options
-      var o = $.meta ? $.extend({}, $settings, $this.data()) : $settings;
-      var $$oldIndex = typeof(o.defaultColor)=='number' ? o.defaultColor : -1;
-      
-      var _html = "";
-      for(i=0;i<o.color.length;i++){
-	_html += '<div style="background-color:'+o.color[i]+';"></div>';
-	if($$oldIndex==-1 && o.defaultColor==o.color[i]) $$oldIndex = i;
-      }
-      
-      $this.html('<div class="jColorSelect">'+_html+'</div>');
-      var $color = $this.children('.jColorSelect').children('div');
-      // Set container width
-      var w = ($color.width()+2+2) * (o.columns>0 ? o.columns : o.color.length );
-      //$this.children('.jColorSelect').css('width',w);
-      
-      // Subscribe to click event of each color box
-      $color.each(function(i){
-	$(this).click(function(){
-	  if( $$oldIndex == i ) return;	  
-	  if( $$oldIndex > -1 ){
-	    cell = $color.eq($$oldIndex);
-	    if(cell.hasClass('check')) cell.removeClass(
-	      'check').removeClass('checkwht').removeClass('checkblk');
-	  }
-	  // Keep index
-	  $$oldIndex = i;
-	  $(this).addClass('check').addClass(isdark(o.color[i]) ? 'checkwht' : 'checkblk');
-	  // Trigger user event
-	  o.click(o.color[i]);
-	});
-      });
-      
-      // Simulate click for defaultColor
-      _tmp = $$oldIndex;
-      $$oldIndex = -1;
-      $color.eq(_tmp).trigger('click');
-    });    
-    return this;
+      var colorDivs=$this.find(".jColorSelect > div")
+      var result=[];
+      for (var i=0; i<colorDivs.length; i++) {
+      	result.push(rgb2hex($(colorDivs[i]).css("background-color")))
+      };
+      return result;
+    }
+    
+  };	
+	
+  $.fn.colorPalette = function(method) {
+    if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' is not supported for jQuery.colorPalette' );
+    } 
   };
-  
-  
 })(jQuery);
 
 /**
@@ -104,4 +124,20 @@ function isdark(color){
     + ((colr >>> 8) & 0x00ff) // G 
     + (colr & 0x0000ff) // B
     < 500;
+}
+
+function rgb2hex(rgb) {
+     if (  rgb.search("rgb") == -1 ) {
+          return rgb;
+     }
+     else if ( rgb == 'rgba(0, 0, 0, 0)' ) {
+         return 'transparent';
+     }
+     else {
+          rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
+          function hex(x) {
+               return ("0" + parseInt(x).toString(16)).slice(-2);
+          }
+          return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]); 
+     }
 }
