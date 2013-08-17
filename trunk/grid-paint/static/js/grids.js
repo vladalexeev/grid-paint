0b27c7191ef78cd1859d19c29b45c3d5004089b4
vlad.alexeev@gmail.com <vlad.alexeev@gmail.com@83b7ce2f-46e5-b878-b427-490d49653a27>
2013-08-17 18:54:17 +0000

    Palette of currently selected color and 16 recent colors.

--- trunk/grid-paint/static/js/grids.js
+++ trunk/grid-paint/static/js/grids.js
@@ -36,6 +36,26 @@ function hslToHex(h,s,l) {
 	return Raphael.hsl(h,s,l);
 }
 
+/*
+ * Convert string ing format 'rgb(25,30,165)' to hex color string
+ */
+function rgb2hex(rgb) {
+     if (  rgb.search("rgb") == -1 ) {
+          return rgb;
+     }
+     else if ( rgb == 'rgba(0, 0, 0, 0)' ) {
+         return 'transparent';
+     }
+     else {
+          rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
+          function hex(x) {
+               return ("0" + parseInt(x).toString(16)).slice(-2);
+          }
+          return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]); 
+     }
+}
+
+
 function GridArtwork() {
     this.cells=[];
     
