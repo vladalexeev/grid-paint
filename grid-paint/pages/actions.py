2ea53110685215940763f9fe02d404993dd17664
Vlad Alexeev <vlad.alexeev@gmail.com>
2016-07-29 22:07:33 +0300

    Admin page for iteratable update of artworks

--- grid-paint/pages/actions.py
+++ grid-paint/pages/actions.py
@@ -540,33 +540,46 @@ class ActionUpdateArtworkIterate(BasicRequestHandler):
         if not self.user_info.superadmin:
             self.response.set_status(403)
             return
-
-        offset = int(self.request.get('offset'))
-        limit = 20
         
-        artworks = db.Artwork.all().order('-date').fetch(limit, offset)
+        year1 = int(self.request.get('year1'))
+        month1 = int(self.request.get('month1'))
+        day1 = int(self.request.get('day1'))
+        year2 = int(self.request.get('year2'))
+        month2 = int(self.request.get('month2'))
+        day2 = int(self.request.get('day2'))
         
-
-    
-        for a in artworks:
-            filename = '/images/png/'+str(a.key().id())+'.png'
-            a.full_image_file_name = filename
-            cs.create_file(filename, 'image/png', a.full_image)
-            
-            filename_small = '/images/png/'+str(a.key().id())+'-small.png'
-            a.small_image_file_name = filename_small
-            cs.create_file(filename_small, 'image/png', a.small_image)
-            
-            a.put()
-            
-            
+        date1 = datetime.datetime(year=year1, month=month1, day=day1)
         
-        self.response.headers['Content-Type']='text/plain'
+        date2 = datetime.datetime(year=year2, month=month2, day=day2)
         
-        if len(artworks) == limit:
-            self.response.out.write(str(offset+limit))
-        else:
-            self.response.out.write('end')
+        all_artworks = db.Artwork.all().filter('date >=', date1).filter('date <=', date2).fetch(1000,0)
+        total_count = 0
+        updated_count = 0
+        skipped_count = 0
+        error_count = 0
+        
+        for a in all_artworks:
+            try:
+                total_count = total_count+1
+                if a.author and not a.author_email:
+                    a.author_email = a.author.email()
+                    updated_count = updated_count +1
+                    a.put()
+                else:
+                    skipped_count = skipped_count + 1
+            except Exception as e:
+                import logging
+                logging.error('Error', e)
+                error_count = error_count + 1
+                
+        result = {
+                  'total_count': total_count,
+                  'updated_count': updated_count,
+                  'skipped_count': skipped_count,
+                  'error_count': error_count,
+                  'all_done': total_count<1000
+                  }
+        self.response.write(json.dumps(result))
 
 
                 
